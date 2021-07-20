1e08219320a3a38b8ef285bd46de3a2f9b03ddaf
Joy Lee <purple402@naver.com>
2021-07-20 21:53:11 +0900

    Update using selenium

--- aladdin.py
+++ aladdin.py
@@ -1,5 +1,9 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
+def get_last_page(word):
+    driver = webdriver.Chrome()
+    driver.get("https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=UsedStore&KeyWord=piano&KeyTag=F50+B90")
 def get_books(word):
     last_page = get_last_page(word)
     print(last_page)
