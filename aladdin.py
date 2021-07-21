f2e1346a20769520a75779b195aeb8fa655b52f5
Joy Lee <purple402@naver.com>
2021-07-21 16:11:08 +0900

    Make extrack_books()

--- aladdin.py
+++ aladdin.py
@@ -13,9 +13,17 @@ def get_last_page(word):
         last_page = last_page[-4:-2].replace("'", "")
         return int(last_page)
 
+def extract_books(word, last_page):
+    books = []
+    for page in range(last_page):
+        print(f"scrapping aladdin page {page}")
+        response = requests.get(f"https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=UsedStore&KeyWord={word}&KeyTag=F50+B90&page={page+1}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        results = soup.select("div#Search3_Result > div.ss_book_box")
 
 def get_books(word):
     last_page = get_last_page(word)
     print(last_page)
+    books = extract_books(word, last_page)
 
 get_books('python')
