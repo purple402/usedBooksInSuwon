a7d35b751111be0b8b5e5a7e7d0dab7e42d92342
Joy Lee <purple402@naver.com>
2021-07-21 16:11:28 +0900

    Make get_book_info()

--- aladdin.py
+++ aladdin.py
@@ -13,6 +13,16 @@ def get_last_page(word):
         last_page = last_page[-4:-2].replace("'", "")
         return int(last_page)
 
+def get_book_info(result):
+    image = result.select_one('img')['src']
+    title = result.select_one('div.ss_book_list b.bo3').text
+    stores = result.select('div.usedshop_off_text2_box > a')
+    store_list = []
+    for store in stores:
+        store = store.get_text()
+        store_list.append(store)
+    return {"image": image, "title": title, "stores": store_list}
+
 def extract_books(word, last_page):
     books = []
     for page in range(last_page):
@@ -20,10 +30,14 @@ def extract_books(word, last_page):
         response = requests.get(f"https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=UsedStore&KeyWord={word}&KeyTag=F50+B90&page={page+1}")
         soup = BeautifulSoup(response.text, "html.parser")
         results = soup.select("div#Search3_Result > div.ss_book_box")
+        for result in results:
+            book = get_book_info(result)
+            books.append(book)
+    return books
 
 def get_books(word):
     last_page = get_last_page(word)
-    print(last_page)
     books = extract_books(word, last_page)
+    return books
 
 get_books('python')
