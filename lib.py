6b4746bd471ba17503438fbde135cfbec335f8c5
Joy Lee <purple402@naver.com>
2021-07-21 21:12:20 +0900

    Get driver using selenium

--- lib.py
+++ lib.py
@@ -1,1 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+def make_driver(word):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(3)
+    driver.get('https://www.suwonlib.go.kr:8443/')
+    select = Select(driver.find_element_by_class_name('select-all'))
+    select.select_by_value('SB')
+    elem_search = driver.find_element_by_xpath('//*[@id="search"]')
+    elem_search.send_keys(word)
+    driver.find_element_by_xpath('//*[@id="nav"]/div[1]/div[2]/div/div/button[2]').click()
+    return driver
+
 def get_books(word):
+    driver = make_driver(word)
