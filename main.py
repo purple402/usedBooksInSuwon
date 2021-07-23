432de4e916ecbdfdded6afe478c286c21177e42d
Joy Lee <purple402@naver.com>
2021-07-23 21:50:05 +0900

    Create report page

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from aladdin import get_books as get_books_aladdin
+from lib import get_books as get_books_lib
 
 
 app = Flask(__name__)
@@ -8,6 +10,14 @@ app = Flask(__name__)
 def home():
     return render_template('main.html')
 
+@app.route('/report')
+def report():
+    searchword = request.args.get('searchword')
+    books = []
+    aladdin_books = get_books_aladdin(searchword)
+    lib_books = get_books_lib(searchword)
+    books = aladdin_books + lib_books
+    return render_template('report.html', searchword = searchword, books = books)
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", debug=True)
