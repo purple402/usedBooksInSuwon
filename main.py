112fabdbe6f825a0eb779ec0b80a9857dff484d9
Joy Lee <purple402@naver.com>
2021-07-23 21:48:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
-app = Flask("Used Books in Suwon")
+
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'hello world'
 
-app.run(host="127.0.0.1", debug=True)
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", debug=True)
