a8ca958bef3951240e0484df92b39261d0b838fd
Joy Lee <purple402@naver.com>
2021-07-23 21:49:01 +0900

    Make main page render template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello world'
+    return render_template('main.html')
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", debug=True)
